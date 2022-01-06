542120648100288f93045cf391fab85457ec9388
younggi0123 <younggi0123@naver.com>
2022-01-07 00:16:54 +0900

    Add files via upload

---
+++ extractText_from_beautifulSoup.py
@@ -0,0 +1,59 @@
+# Final code 📃
+# This final code is a slightly different version where script was adjusted to keep paragraph headers as well
+# so that there are two available examples to reference for both approaches.
+# This may be more useful instead of collating the codes already shown in sections above.
+
+# ========== 1. Using wikipedia ==========
+
+
+# Import packages
+import wikipedia
+
+# Specify the title of the Wikipedia page
+wiki = wikipedia.page('John D. Hunter')
+
+# Extract the plain text content of the page, excluding images, tables, and other data.
+text = wiki.content
+
+# Replace '==' with '' (an empty string)
+text = text.replace('==', '')
+
+# Replace '\n' (a new line) with '' & end the string at $1000.
+text = text.replace('\n', '')[:-12]
+print(text)
+
+# ========== 2. Using urllib & BeatifulSoup ==========
+# Import packages
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+# Specify url of the web page
+source = urlopen('https://en.wikipedia.org/wiki/John_D._Hunter').read()
+
+# Make a soup 
+soup = BeautifulSoup(source,'lxml')
+soup
+
+# Extract the plain text content from paragraphs
+paras = []
+for paragraph in soup.find_all('p'):
+    paras.append(str(paragraph.text))
+
+# Extract text from paragraph headers
+heads = []
+for head in soup.find_all('span', attrs={'mw-headline'}):
+    heads.append(str(head.text))
+
+# Interleave paragraphs & headers
+text = [val for pair in zip(paras, heads) for val in pair]
+text = ' '.join(text)
+
+# Drop footnote superscripts in brackets
+text = re.sub(r"\[.*?\]+", '', text)
+
+# Replace '\n' (a new line) with '' and end the string at $1000.
+text = text.replace('\n', '')[:-11]
+
+
+print(text)
